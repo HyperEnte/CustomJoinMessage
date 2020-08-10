c82172cdf2cd33112d92022925e946f2053785dc
HyperEnte <54057755+HyperEnte-PHP@users.noreply.github.com>
2020-08-10 13:36:49 +0200

    Add files via upload

--- JoinMessage/src/HyperEnte/JoinMessage/EventListener.php
+++ JoinMessage/src/HyperEnte/JoinMessage/EventListener.php
@@ -5,10 +5,14 @@ namespace HyperEnte\JoinMessage;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\level\Level;
+use pocketmine\Player;
 use pocketmine\utils\Config;
 use onebone\economyapi\EconomyAPI;
 
 class EventListener implements Listener{
+
 
 	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
@@ -17,6 +21,7 @@ class EventListener implements Listener{
 		if($config->get($player->getName()) == false){
 			$config->set($player->getName(), ["joinmessage" => "default", "leavemessage" => "default"]);
 			$config->save();
+			$event->setJoinMessage(JoinMessage::getMain()->getConfig()->get("default.firstjoin"));
 		}
 		$info = $config->get("$name");
 		if($info["joinmessage"] === "default"){
@@ -42,6 +47,14 @@ class EventListener implements Listener{
 				$info["joinmessage"]
 			);
 			$event->setJoinMessage($joinmsg);
+		}
+		if($config->get($player->getName()) == false){
+			$config->set($player->getName(), ["joinmessage" => "default", "leavemessage" => "default"]);
+			$config->save();
+			$event->setJoinMessage(JoinMessage::getMain()->getConfig()->get("default.firstjoin"));
+		}
+		if(JoinMessage::getMain()->getConfig()->get("teleport.to.spawn") === "true"){
+			$player->teleport($player->getServer()->getDefaultLevel()->getSpawnLocation());
 		}
 	}
 	public function onQuit(PlayerQuitEvent $event){
@@ -72,6 +85,28 @@ class EventListener implements Listener{
 				$info["leavemessage"]
 			);
 			$event->setQuitMessage($joinmsg);
+		}
+	}
+	public function onLevelChange(EntityLevelChangeEvent $event){
+		$entity = $event->getEntity();
+		$name = $entity->getNameTag();
+		$money = EconomyAPI::getInstance()->myMoney($entity);
+		date_default_timezone_set(JoinMessage::getMain()->getConfig()->get("timezone"));
+		$time = date("H:i");
+		$rank = JoinMessage::getMain()->getPlayerRank($entity);
+		$world = $event->getTarget()->getFolderName();
+		if($entity instanceof Player){
+			if(JoinMessage::getMain()->getConfig()->get("enabled")){
+				$jm = JoinMessage::getMain()->getConfig()->get("default.world");
+				$players = $event->getTarget()->getPlayers();
+				foreach ($players as $player) {
+					$joinmsg = str_replace(["[PLAYER]", "[MONEY]", "[TIME]", "[DARK_BLUE]", "[DARK_GREEN]", "[DARK_AQUA]", "[DARK_RED]", "[DARK_PURPLE]", "[GOLD]", "[GRAY]", "[DARK_GRAY]", "[BLUE]", "[BLACK]", "[GREEN]", "[AQUA]", "[RED]", "[LIGHT_PURPLE]", "[YELLOW]", "[WHITE]", "[RANK]", "[WORLD]"],
+						["$name", "$money", "[$time]", "§1", "§2", "§3", "§4", "§5", "§6", "§7", "§8", "§9", "§0", "§a", "§b", "§c", "§d", "§e", "§f", "$rank", "$world"],
+						$jm);
+					$player->sendMessage($joinmsg);
+					$entity->getPlayer()->sendMessage($joinmsg);
+				}
+			}
 		}
 	}
 }
--- JoinMessage/src/HyperEnte/JoinMessage/JoinMessage.php
+++ JoinMessage/src/HyperEnte/JoinMessage/JoinMessage.php
@@ -4,6 +4,7 @@ namespace HyperEnte\JoinMessage;
 
 use HyperEnte\JoinMessage\commands\DefaultLeaveMessageCommand;
 use HyperEnte\JoinMessage\commands\DefaultMessageCommand;
+use HyperEnte\JoinMessage\commands\JoinCommand;
 use HyperEnte\JoinMessage\commands\JoinMessageCommand;
 use HyperEnte\JoinMessage\commands\LeaveMessageCommand;
 use pocketmine\Player;
@@ -20,16 +21,17 @@ class JoinMessage extends PluginBase
 
 	public function onEnable(){
 		self::$main = $this;
-		if($this->getConfig()->get("version") != "2"){
+		if($this->getConfig()->get("version") != "3"){
 			$this->getServer()->getLogger()->info("\n\nYour config file was outdated. Please restart the server\n");
-			unlink($this->getDataFolder() . "joinmessages.json");
 			unlink($this->getDataFolder() . "config.yml");
 			return;
 		}
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
 		$config = new Config($this->getDataFolder() . "joinmessages.json", Config::JSON);
 		$config->save();
-		$this->getServer()->getCommandMap()->registerAll("JoinMessage", [new JoinMessageCommand(), new DefaultMessageCommand(), new LeaveMessageCommand(), new DefaultLeaveMessageCommand()]);
+		$worldmsg = new Config($this->getDataFolder() . "worldmessages.json", Config::JSON);
+		$worldmsg->save();
+		$this->getServer()->getCommandMap()->registerAll("JoinMessage", [new JoinMessageCommand(), new DefaultMessageCommand(), new LeaveMessageCommand(), new DefaultLeaveMessageCommand(), new JoinCommand()]);
 		$this->purePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
 	}
 
@@ -45,5 +47,11 @@ class JoinMessage extends PluginBase
 		}else{
 			return "No Rank";
 		}
+	}
+	public function getJoinMessage(Player $player){
+		$name = $player->getName();
+		$config = new Config(JoinMessage::getMain()->getDataFolder()."joinmessages.json", Config::JSON);
+		$info = $config->get("$name");
+		$info["joinmessage"];
 	}
 }
